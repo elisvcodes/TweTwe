17b31fb95228351c2951ee128bc6043e49be28cd
Eli Sultanov <elisultanov@Elis-MacBook-Air.local>
2021-02-08 13:36:19 -0500

    Added the update route as well as the like route

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createPost } = require('../controller/posts');
+const { createPost, likePost, updatePost } = require('../controller/post');
 const auth = require('../middleware/auth');
 
 router.post('/', auth, createPost);
-
+router.patch('/:id/like', auth, likePost);
+router.patch('/:id', updatePost);
 module.exports = router;
