aa35fe071e6803cbb27f4edbb7084ffbd8695517
Eli Sultanov <elisultanov@Elis-MacBook-Air.local>
2021-02-08 14:49:24 -0500

    delete post route

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, likePost, updatePost } = require('../controller/post');
+const {
+  createPost,
+  likePost,
+  updatePost,
+  deletePost,
+} = require('../controller/post');
 const auth = require('../middleware/auth');
 
 router.post('/', auth, createPost);
 router.patch('/:id/like', auth, likePost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 module.exports = router;
