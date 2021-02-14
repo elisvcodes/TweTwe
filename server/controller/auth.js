6bcc0fd9cd7bc145ed74806cb397e49c7206b9a1
Eli Sultanov <elisultanov@Elis-MacBook-Air.local>
2021-02-13 19:38:42 -0500

    logout controller

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -5,10 +5,23 @@ const logIn = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findByEmail(email, password);
     user.generateToken();
-    res.status(200).json(user);
+    const token = user.tokens[user.tokens.length - 1].token;
+    res.status(200).json({ result: user, token });
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({ error });
   }
 };
 
-module.exports = { logIn };
+const logOut = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
+    await req.user.save();
+    res.status(200).json('logged out!');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { logIn, logOut };
