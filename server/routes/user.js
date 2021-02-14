e757e4a04a718f10ee40f82862f070ec81b9d05d
Eli Sultanov <elisultanov@Elis-MacBook-Air.local>
2021-02-13 19:34:54 -0500

    minor tweaks

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('../controller/users');
+const { createUser } = require('../controller/user');
 
 router.post('/', createUser);
 
