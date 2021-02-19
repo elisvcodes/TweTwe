0cb4a4b2d8ab73a7c404c67c75d3d00f979def35
elisvcodes <sultanoveli@outlook.com>
2021-02-19 15:35:33 -0500

    API for the user part

---
+++ client/src/api/user.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.authorization = `Bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`;
+  }
+  return req;
+});
+
+// const API = axios.create({ baseURL: 'http://localhost:8000' });
+
+export const signUp = (data) => axios.post(`http://localhost:8000/users`, data);
+export const getUserPosts = (id) =>
+  axios.get(`http://localhost:8000/posts/${id}`);
+export const likePost = (id) =>
+  axios.patch(`http://localhost:8000/posts/${id}/like`);
