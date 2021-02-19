82c123fdb6a25461b4d60cf61e192c45fea901a2
elisvcodes <sultanoveli@outlook.com>
2021-02-19 15:37:28 -0500

    Reducer

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import user from './user';
+import posts from './posts';
 export default combineReducers({
   auth,
+  user,
+  posts,
 });
