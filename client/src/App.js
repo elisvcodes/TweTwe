acfd4b2d318132bcfc280bf08889f3ca104093de
elisvcodes <sultanoveli@outlook.com>
2021-02-19 15:38:45 -0500

    Cleaned up App.js and included a few libraries

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Auth from './components/auth/Auth';
+import { login, logout } from './_actions/auth';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/user/Profile';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {}, []);
+  const data = { email: 'j@a.com', password: 'j' };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signin" component={Auth} />
+        <Route path="/signup" component={Auth} />
+        <Route path="/:id" component={Profile} />
+      </Switch>
+    </>
   );
 }
 
