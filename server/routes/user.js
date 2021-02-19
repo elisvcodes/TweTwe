d02661e53366ca33c09d9a89ccbe8fea7362ad7b
elisvcodes <sultanoveli@outlook.com>
2021-02-19 16:35:21 -0500

    Delete user account route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, updateUser } = require('../controller/user');
+const { createUser, updateUser, deleteUser } = require('../controller/user');
 const auth = require('../middleware/auth');
 
 router.post('/', createUser);
 router.patch('/update', auth, updateUser);
+router.delete('/deletemyaccount', auth, deleteUser);
 module.exports = router;
