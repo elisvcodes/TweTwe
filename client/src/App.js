537bbcab5bd240545acd07cfb025db8493050f0d
elisvcodes <sultanoveli@outlook.com>
2021-02-21 14:10:07 -0500

    minor tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Auth from './components/auth/Auth';
-import { login, logout } from './_actions/auth';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/user/Profile';
+import CreatePost from './components/posts/CreatePost';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {}, []);
-  const data = { email: 'j@a.com', password: 'j' };
   return (
     <>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/signin" component={Auth} />
         <Route path="/signup" component={Auth} />
+        <Route path="/compose" component={CreatePost} />
         <Route path="/:id" component={Profile} />
       </Switch>
     </>
