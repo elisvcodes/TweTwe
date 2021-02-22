9ddc9cdae34cf99e7a4077e8a8cd0307e832ed8a
elisvcodes <sultanoveli@outlook.com>
2021-02-22 16:22:13 -0500

    Redirecting users when they create a new post

--- client/src/components/posts/CreatePost.js
+++ client/src/components/posts/CreatePost.js
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createPost } from '../../_actions/user';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+
 export default function CreatePost() {
   const [user, setuser] = useState(JSON.parse(localStorage.getItem('profile')));
   const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
+  let history = useHistory();
+
   const onSubmit = (data) => {
     data.author = user.result._id;
     console.log(data);
     dispatch(createPost(data));
     reset();
+    setTimeout(() => {
+      history.push(`/${user.result._id}`);
+    }, 100);
   };
   return (
     <>
