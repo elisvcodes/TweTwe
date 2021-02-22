b13c610e38cc98ecf23c2e6854a7a822c18543e8
elisvcodes <sultanoveli@outlook.com>
2021-02-22 16:21:48 -0500

    Added a few operation for the user profile

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import GetUserPosts from '../posts/GetUserPosts';
+import { useHistory } from 'react-router-dom';
+import { logout } from '../../_actions/auth';
+import { useDispatch } from 'react-redux';
 
 export default function Profile(props) {
+  const history = useHistory();
+  const dispatch = useDispatch();
   return (
     <>
       <h1> User Profile</h1>
+      <button
+        onClick={() => {
+          dispatch(logout());
+          setTimeout(() => {
+            history.push('/');
+          }, 100);
+        }}
+      >
+        Logout
+      </button>
+      <button onClick={() => history.push('/compose')}>Compose</button>
       <GetUserPosts id={props.match.params.id} />
     </>
   );
