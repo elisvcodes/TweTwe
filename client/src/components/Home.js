4319c005859ccbd591fe7a7f44ad54cfcaea60f2
elisvcodes <sultanoveli@outlook.com>
2021-03-17 11:31:20 -0400

    Styled the HomePage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,42 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Grid, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    background: '#e7ecef',
+    width: '100%',
+    minHeight: '100vh',
+  },
+  headerText: {
+    fontWeight: 'normal',
+  },
+  subHeaderText: {
+    fontSize: '18px',
+    letterSpacing: '2px',
+    lineHeight: '30px',
+  },
+  buttonsPosition: {
+    textAlign: 'center',
+    margin: '30px 0px',
+  },
+  buttons: {
+    border: 'none',
+    background: '#ef8354',
+    color: 'white',
+    padding: '10px 30px',
+    fontSize: '15px',
+    letterSpacing: '2px',
+    textTransform: 'uppercase',
+    '&:hover': {
+      background: '#eb5e28',
+      cursor: 'pointer',
+    },
+  },
+});
 
 export default function Home() {
   const [user, setuser] = useState(JSON.parse(localStorage.getItem('profile')));
@@ -10,27 +47,45 @@ export default function Home() {
     isJoined = false;
   }
 
+  const classes = useStyles();
+
   return (
-    <>
-      <h1>{isJoined ? `Welcome ${user.result.name}` : `Welcome to TweTwe`}</h1>
-      {isJoined ? (
-        <Link to={user.result._id}>
-          <button> View Profile</button>
-        </Link>
-      ) : (
-        <>
-          <div>
-            <Link to="/signup">
-              <button> Sign up</button>
+    <div className={classes.root}>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <h1 className={classes.headerText}>Welcome to TweTwe</h1>
+            <p className={classes.subHeaderText}>
+              TweTwe a social media platform that allows users to share their
+              opinions freely and openly.
+            </p>
+          </Grid>
+          {isJoined ? (
+            <Link to={user.result._id}>
+              <button className={classes.buttons}> View Profile</button>
             </Link>
-          </div>
-          <div>
-            <Link to="/signin">
-              <button> Sign In</button>
-            </Link>
-          </div>
-        </>
-      )}
-    </>
+          ) : (
+            <>
+              <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={6} className={classes.buttonsPosition}>
+                  <div>
+                    <Link to="/signup">
+                      <button className={classes.buttons}> Sign up</button>
+                    </Link>
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={6} className={classes.buttonsPosition}>
+                  <div>
+                    <Link to="/signin">
+                      <button className={classes.buttons}> Sign In</button>
+                    </Link>
+                  </div>
+                </Grid>
+              </Grid>
+            </>
+          )}
+        </Grid>
+      </Container>
+    </div>
   );
 }
