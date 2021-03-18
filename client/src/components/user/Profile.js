7ddf22a2ac5330b30280d5f788793a17c45e16b6
elisvcodes <sultanoveli@outlook.com>
2021-03-17 20:22:06 -0400

    Organized and styled the profile page

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GetUserPosts from '../posts/GetUserPosts';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../_actions/auth';
 import { useDispatch } from 'react-redux';
+import { Container, Grid } from '@material-ui/core';
 
 export default function Profile(props) {
   const history = useHistory();
   const dispatch = useDispatch();
+  const user = useState(JSON.parse(localStorage.getItem('profile')));
+  console.log(user);
+  if (user[0] == null) {
+    history.push('/signin');
+  }
   return (
     <>
-      <h1> User Profile</h1>
-      <button
-        onClick={() => {
-          dispatch(logout());
-          setTimeout(() => {
-            history.push('/');
-          }, 100);
-        }}
-      >
-        Logout
-      </button>
-      <button onClick={() => history.push('/compose')}>Compose</button>
-      <GetUserPosts id={props.match.params.id} />
+      {user && (
+        <>
+          <h1> User Profile</h1>
+          <button
+            onClick={() => {
+              dispatch(logout());
+              setTimeout(() => {
+                history.push('/');
+              }, 100);
+            }}
+          >
+            Logout
+          </button>
+          <button onClick={() => history.push('/compose')}>Compose</button>
+          <Container>
+            <Grid container>
+              <Grid item xs={12} sm={3}>
+                <h1>Hello</h1>
+              </Grid>
+              <Grid item xs={12} sm={9}>
+                <GetUserPosts id={props.match.params.id} />
+              </Grid>
+            </Grid>
+          </Container>
+        </>
+      )}
     </>
   );
 }
