74b408f7481dd33474f45d7a8eaac0f5ec7f1596
elisvcodes <sultanoveli@outlook.com>
2021-03-18 18:29:34 -0400

    add the follow functionality to redux

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import user from './user';
 import posts from './posts';
+import follow from './follow';
 export default combineReducers({
   auth,
   user,
   posts,
+  follow,
 });
