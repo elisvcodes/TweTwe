efddd04f952c3dbee18c17ab35ad3007386a52dd
elisvcodes <sultanoveli@outlook.com>
2021-03-18 18:27:50 -0400

    removed extra code

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -5,15 +5,12 @@ import { logout } from '../../_actions/auth';
 import { useDispatch } from 'react-redux';
 import { Container, Grid } from '@material-ui/core';
 import ProfileHeader from './ProfileHeader';
+import FollowCounter from './FollowCounter';
 
 export default function Profile(props) {
   const history = useHistory();
   const dispatch = useDispatch();
   const user = useState(JSON.parse(localStorage.getItem('profile')));
-  console.log(user);
-  if (user[0] == null) {
-    history.push('/signin');
-  }
   return (
     <>
       {user && (
@@ -34,7 +31,10 @@ export default function Profile(props) {
             <ProfileHeader user={user} />
             <Grid container>
               <Grid item xs={12} sm={3}>
-                <h1>Hello</h1>
+                <FollowCounter
+                  follower={user[0].result._id}
+                  following={props.match.params.id}
+                />
               </Grid>
               <Grid item xs={12} sm={9}>
                 <GetUserPosts id={props.match.params.id} />
