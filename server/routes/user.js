5a25971c1c8b5290205d069a6a51a39164e3b24b
elisvcodes <sultanoveli@outlook.com>
2021-03-19 21:47:50 -0400

    singleUser route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, updateUser, deleteUser } = require('../controller/user');
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+  getSingleUser,
+} = require('../controller/user');
 const auth = require('../middleware/auth');
 
+router.get('/:id', getSingleUser);
 router.post('/', createUser);
 router.patch('/update', auth, updateUser);
 router.delete('/deletemyaccount', auth, deleteUser);
