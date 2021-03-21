fe3d53630975f7ff4ce1dff1d920b5da98cc8ab2
elisvcodes <sultanoveli@outlook.com>
2021-03-20 22:56:37 -0400

    follow, unfollow and following API

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -25,3 +25,9 @@ export const followingCount = (following) =>
   axios.post('http://localhost:8000/follow/followingcount', following);
 export const followerCount = (follower) =>
   axios.post('http://localhost:8000/follow/followercount', follower);
+export const follow = (userIds) =>
+  axios.post('http://localhost:8000/follow', userIds);
+export const unfollow = (userIds) =>
+  axios.post('http://localhost:8000/follow/unfollow', userIds);
+export const following = (userIds) =>
+  axios.post('http://localhost:8000/follow/following', userIds);
