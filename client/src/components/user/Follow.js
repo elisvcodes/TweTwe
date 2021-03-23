52a9b0bd148158f3fbcd4310b3707606ae5cfa05
elisvcodes <sultanoveli@outlook.com>
2021-03-23 18:04:59 -0400

    Styled & made the follow button functional

--- client/src/components/user/Follow.js
+++ client/src/components/user/Follow.js
@@ -7,15 +7,33 @@ import {
   getFollowingCount,
 } from '../../_actions/user';
 import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  button: {
+    border: 'none',
+    background: '#ef8354',
+    color: 'white',
+    padding: '10px 7px',
+    maxWidth: '95px',
+    fontSize: '12px',
+    letterSpacing: '2px',
+    textTransform: 'uppercase',
+    '&:hover': {
+      background: '#eb5e28',
+      cursor: 'pointer',
+    },
+  },
+});
+
 export default function Follow({ user, viewingProfile }) {
   const dispatch = useDispatch();
   const isFollowing = useSelector((state) => state.follow.isFollowing);
-  const [followingOption, setFollowingOption] = useState(isFollowing);
+  const [followingOption, setFollowingOption] = useState(false);
   useEffect(() => {
     dispatch(following({ following: viewingProfile._id, follower: user._id }));
     setFollowingOption(isFollowing);
-  });
-
+  }, [dispatch, isFollowing]);
   const trackButton = () => {
     followingOption
       ? dispatch(
@@ -23,6 +41,11 @@ export default function Follow({ user, viewingProfile }) {
             following: viewingProfile._id,
             follower: user._id,
           }),
+          dispatch(
+            getFollowerCount({
+              following: viewingProfile._id,
+            })
+          ),
           setFollowingOption(!followingOption)
         )
       : dispatch(
@@ -30,39 +53,21 @@ export default function Follow({ user, viewingProfile }) {
             following: viewingProfile._id,
             follower: user._id,
           }),
+          dispatch(
+            getFollowerCount({
+              following: viewingProfile._id,
+            })
+          ),
           setFollowingOption(!followingOption)
         );
   };
+  const classes = useStyles();
 
   return (
     <>
-      <button onClick={trackButton}>
+      <button onClick={trackButton} className={classes.button}>
         {followingOption ? 'unfollow' : 'follow'}
       </button>
     </>
   );
-}
-
-{
-  /* <button
-onClick={() =>
-  followingOption
-    ? dispatch(
-        unfollow({
-          following: viewingProfile._id,
-          follower: user._id,
-        }),
-        setFollowingOption(!followingOption)
-      )
-    : dispatch(
-        follow({
-          following: viewingProfile._id,
-          follower: user._id,
-        }),
-        setFollowingOption(!followingOption)
-      )
-}
->
-{followingOption ? 'unfollow' : 'follow'}
-</button> */
 }
