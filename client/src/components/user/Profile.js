5d4f7da807bd56f1a8ee202603b82a20a4bcf9cc
elisvcodes <sultanoveli@outlook.com>
2021-03-23 18:06:10 -0400

    Improved the profile component code

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import GetUserPosts from '../posts/GetUserPosts';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../_actions/auth';
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Container, Grid } from '@material-ui/core';
 import ProfileHeader from './ProfileHeader';
 import FollowCounter from './FollowCounter';
+import { getFollowerCount, getFollowingCount } from '../../_actions/user';
 
 export default function Profile(props) {
   const userId = props.match.params.id;
@@ -16,40 +17,38 @@ export default function Profile(props) {
   const user = useSelector((state) => state.user);
   useEffect(() => {
     dispatch(getSingleUser(userId));
+    dispatch(getFollowerCount({ following: userId }));
+    dispatch(getFollowingCount({ follower: userId }));
   }, [dispatch]);
 
   return (
     <>
-      {user && (
-        <>
-          <h1> User Profile</h1>
-          <button
-            onClick={() => {
-              dispatch(logout());
-              setTimeout(() => {
-                history.push('/signin');
-              }, 300);
-            }}
-          >
-            Logout
-          </button>
-          <button onClick={() => history.push('/compose')}>Compose</button>
-          <Container>
-            <ProfileHeader
-              user={loggedInUser ? loggedInUser.result : user.userData}
-              viewingProfile={user.userData}
-            />
-            <Grid container>
-              <Grid item xs={12} sm={3}>
-                <FollowCounter follower={userId} following={userId} />
-              </Grid>
-              <Grid item xs={12} sm={9}>
-                <GetUserPosts id={userId} />
-              </Grid>
-            </Grid>
-          </Container>
-        </>
-      )}
+      <h1> User Profile</h1>
+      <button
+        onClick={() => {
+          dispatch(logout());
+          setTimeout(() => {
+            history.push('/signin');
+          }, 300);
+        }}
+      >
+        Logout
+      </button>
+      <button onClick={() => history.push('/compose')}>Compose</button>
+      <Container>
+        <ProfileHeader
+          user={loggedInUser ? loggedInUser.result : user.userData}
+          viewingProfile={user.userData}
+        />
+        <Grid container>
+          <Grid item xs={12} sm={3}>
+            <FollowCounter />
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <GetUserPosts id={userId} />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
