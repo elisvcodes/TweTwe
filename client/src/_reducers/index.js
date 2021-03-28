71b2d8c95e9343f2736a5007d97479b6c22e9cf3
elisvcodes <sultanoveli@outlook.com>
2021-03-28 18:46:27 -0400

    Added Single Post and comments to Index Reducer

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -3,9 +3,13 @@ import auth from './auth';
 import user from './user';
 import posts from './posts';
 import follow from './follow';
+import post from './singlePost';
+import comments from './comments';
 export default combineReducers({
   auth,
   user,
   posts,
   follow,
+  post,
+  comments,
 });
