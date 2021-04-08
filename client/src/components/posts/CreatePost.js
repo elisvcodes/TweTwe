0704ec485750802d55cfd24aacb5406dbc71e312
elisvcodes <sultanoveli@outlook.com>
2021-04-08 19:39:58 -0400

    Modified the way I get users input

--- client/src/components/posts/CreatePost.js
+++ client/src/components/posts/CreatePost.js
@@ -3,31 +3,93 @@ import { useDispatch } from 'react-redux';
 import { createPost } from '../../_actions/posts';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Button,
+  TextField,
+  makeStyles,
+} from '@material-ui/core/';
 
-export default function CreatePost() {
-  const [user, setuser] = useState(JSON.parse(localStorage.getItem('profile')));
+const useStyles = makeStyles({
+  root: {
+    marginTop: '10px',
+    paddingRight: '10px',
+  },
+});
+export default function CreatePost({ user }) {
+  const [open, setOpen] = useState(false);
+
   const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
-  let history = useHistory();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const onSubmit = (data) => {
     data.author = user.result._id;
     dispatch(createPost(data));
     reset();
-    setTimeout(() => {
-      history.push(`/${user.result._id}`);
-    }, 100);
   };
+
+  const classes = useStyles();
+
   return (
     <>
+      <div className={classes.root}>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={handleClickOpen}
+          color="white"
+        >
+          Post
+        </Button>
+      </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          name="article"
-          ref={register}
-          placeholder="What's on your mind?"
-        />
-        <button type="submit"> Submit</button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+          fullWidth
+          disablePortal={true}
+        >
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="article"
+              label="What's On Your Mind?"
+              type="text"
+              fullWidth
+              inputRef={register}
+              multiline
+              rows={8}
+              autoComplete="false"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              onClick={() => {
+                handleClose();
+              }}
+              color="primary"
+            >
+              Post
+            </Button>
+          </DialogActions>
+        </Dialog>
       </form>
     </>
   );
