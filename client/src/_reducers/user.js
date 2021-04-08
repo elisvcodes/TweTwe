a793331f99fe0db7d05c19ea87e1335b039ca949
elisvcodes <sultanoveli@outlook.com>
2021-04-08 18:34:09 -0400

    Removed the signup case from user reducer

--- client/src/_reducers/user.js
+++ client/src/_reducers/user.js
@@ -1,8 +1,5 @@
 export default (user = { userData: null }, action) => {
   switch (action.type) {
-    case 'SIGNUP':
-      localStorage.setItem('profile', JSON.stringify(action.payload));
-      return { ...user, userData: action.payload };
     case 'GET_SINGLE_USER':
       return { ...user, userData: action.payload };
     default:
