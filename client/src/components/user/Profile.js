09436495a18ce59413f91f30e7730f3760c07872
elisvcodes <sultanoveli@outlook.com>
2021-04-08 19:42:28 -0400

    Modified the Profile component

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,51 +1,50 @@
 import React, { useEffect } from 'react';
 import GetUserPosts from '../posts/GetUserPosts';
 import { useHistory } from 'react-router-dom';
-import { logout } from '../../_actions/auth';
 import { getSingleUser } from '../../_actions/user';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Grid } from '@material-ui/core';
+import { Button, Container, Grid, makeStyles } from '@material-ui/core';
 import ProfileHeader from './ProfileHeader';
 import FollowCounter from './FollowCounter';
 import { getFollowerCount, getFollowingCount } from '../../_actions/user';
+import { getUserPosts } from '../../_actions/posts';
+import CreatePost from '../posts/CreatePost';
+const useStyles = makeStyles({
+  root: {
+    marginTop: '50px',
+  },
+});
 
 export default function Profile(props) {
-  const userId = props.match.params.id;
-  const loggedInUser = JSON.parse(localStorage.getItem('profile'));
+  const { user, match } = props;
+  const userId = match.params.id;
+  const loggedInUser = user;
   const dispatch = useDispatch();
-  const history = useHistory();
-  const user = useSelector((state) => state.user);
+
+  const usersProfileData = useSelector((state) => state.user);
+  const posts = useSelector((state) => state.posts);
   useEffect(() => {
     dispatch(getSingleUser(userId));
     dispatch(getFollowerCount({ following: userId }));
     dispatch(getFollowingCount({ follower: userId }));
+    dispatch(getUserPosts(userId));
   }, [dispatch]);
 
+  const classes = useStyles();
   return (
     <>
-      <h1> User Profile</h1>
-      <button
-        onClick={() => {
-          dispatch(logout());
-          setTimeout(() => {
-            history.push('/signin');
-          }, 300);
-        }}
-      >
-        Logout
-      </button>
-      <button onClick={() => history.push('/compose')}>Compose</button>
-      <Container>
+      <Container className={classes.root}>
         <ProfileHeader
-          user={loggedInUser ? loggedInUser.result : user.userData}
-          viewingProfile={user.userData}
+          user={loggedInUser ? loggedInUser : usersProfileData}
+          viewingProfile={usersProfileData.userData}
         />
         <Grid container>
           <Grid item xs={12} sm={3}>
             <FollowCounter />
+            <CreatePost user={user} />
           </Grid>
           <Grid item xs={12} sm={9}>
-            <GetUserPosts id={userId} />
+            <GetUserPosts posts={posts} user={user} />
           </Grid>
         </Grid>
       </Container>
