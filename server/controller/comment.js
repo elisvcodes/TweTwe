4bdeab94d696b5b2d8140236bd4f477499b25138
elisvcodes <sultanoveli@outlook.com>
2021-04-08 19:43:09 -0400

    Rewrote the way I create and get comments

--- server/controller/comment.js
+++ server/controller/comment.js
@@ -1,24 +1,28 @@
 const Comment = require('../models/comment');
+const Post = require('../models/post');
 
 const createComment = async (req, res) => {
   const comment = new Comment(req.body);
   comment.save((err, comment) => {
     if (err) return res.json(err);
-    Comment.find({ _id: comment._id })
-      .populate('author')
-      .exec((err, result) => {
+    Post.findOne({ _id: req.body.postId }).exec((err, result) => {
+      if (err) return res.json(err);
+      comment.populate('author').execPopulate((err, comment) => {
         if (err) return res.json(err);
-        res.status(201).json(result);
+        result.comments = result.comments.concat(comment._id);
+        result.save();
+        res.status(201).json(comment);
       });
+    });
   });
 };
 
 const getComments = async (req, res) => {
   Comment.find({ postId: req.body.postId })
     .populate('author')
-    .exec((err, comments) => {
+    .exec((err, post) => {
       if (err) return res.json(err);
-      res.status(200).json(comments);
+      res.status(201).json(post);
     });
 };
 
