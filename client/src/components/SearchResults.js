26a15b6916c9606ab864eb8231617ef069b89c66
elisvcodes <sultanoveli@outlook.com>
2021-04-09 22:17:12 -0400

    Created the search results component

---
+++ client/src/components/SearchResults.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import SinglePost from './posts/SinglePost';
+import { useDispatch, useSelector } from 'react-redux';
+import { search } from '../_actions/search';
+import { Container } from '@material-ui/core';
+export default function SearchResults(props) {
+  const { user } = props;
+  const results = useSelector((state) => state.posts);
+  return (
+    <>
+      <Container>
+        <SinglePost posts={results} user={user} />
+      </Container>
+    </>
+  );
+}
