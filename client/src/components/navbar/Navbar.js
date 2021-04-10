525366719d678e844cbcda06e94282800af7547e
elisvcodes <sultanoveli@outlook.com>
2021-04-09 22:17:29 -0400

    Added a functional search bar to nav

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../_actions/auth';
+import { search } from '../../_actions/search';
+
 import {
   AppBar,
   Toolbar,
@@ -8,6 +10,7 @@ import {
   Button,
   makeStyles,
   Container,
+  InputBase,
 } from '@material-ui/core';
 import { NavLink, useHistory } from 'react-router-dom';
 const useStyles = makeStyles((theme) => ({
@@ -26,12 +29,43 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  inputRoot: {
+    color: '#000',
+    backgroundColor: '#fff',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(0)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
 }));
 
 export default function Navbar({ user, setUser }) {
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const [query, setSearch] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(search(query));
+    history.push(`/search/${query}`);
+  };
 
   return (
     <div>
@@ -43,6 +77,19 @@ export default function Navbar({ user, setUser }) {
                 TweTwe
               </NavLink>
             </Typography>
+            <div className={classes.search}>
+              <form onSubmit={onSubmit}>
+                <InputBase
+                  placeholder="Search…"
+                  onChange={(e) => setSearch(e.target.value)}
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </form>
+            </div>
             {user?.result ? (
               <>
                 <NavLink to={`/${user.result._id}`} className={classes.links}>
