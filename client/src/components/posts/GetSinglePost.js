40a1fbe1d77f303ddb13b622b9081a6a31be5841
elisvcodes <sultanoveli@outlook.com>
2021-04-13 17:26:28 -0400

    changed to using a reusable component

--- client/src/components/posts/GetSinglePost.js
+++ client/src/components/posts/GetSinglePost.js
@@ -1,24 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSingleUserPost } from '../../_actions/posts';
-import { likePost } from '../../_actions/posts';
 import { getComments } from '../../_actions/comments';
-import {
-  Card,
-  CardContent,
-  Container,
-  makeStyles,
-  CardActions,
-  FormControlLabel,
-  Checkbox,
-} from '@material-ui/core';
+import { Card, CardContent, Container, makeStyles } from '@material-ui/core';
 import { Favorite, FavoriteBorder, Comment } from '@material-ui/icons/';
 import * as dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import CreateComment from '../comments/CreateComment';
 import GetComments from '../comments/GetComments';
-import SinglePostData from './SinglePostData';
-import CommentIcon from './CommentIcon';
+import SinglePost from './SinglePost';
 
 dayjs.extend(relativeTime);
 
@@ -41,55 +31,27 @@ export default function GetSinglePost(props) {
     dispatch(getComments({ postId: postid }));
   }, [dispatch]);
 
-  const post = useSelector((state) => state.post);
+  const post = useSelector((state) => state.posts);
   const comments = useSelector((state) => state.comments);
   const classes = useStyles();
-  const checkId =
-    Object.keys(post).length === 0 ? null : post.likes.includes(user._id);
-
   return (
     <>
       <Container className={classes.root}>
-        <Card>
-          {Object.keys(post).length === 0 ? (
-            <h6> Empty</h6>
-          ) : (
-            <>
-              <SinglePostData post={post} />
-              <CardActions>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      icon={
-                        checkId ? (
-                          <Favorite color="primary" />
-                        ) : (
-                          <FavoriteBorder color="secondary" />
-                        )
-                      }
-                      checkedIcon={
-                        checkId ? (
-                          <Favorite color="primary" />
-                        ) : (
-                          <FavoriteBorder color="secondary" />
-                        )
-                      }
-                      name="checkedH"
-                      onClick={() => dispatch(likePost(post._id))}
-                    />
-                  }
-                  label={post.likes.length}
-                />
-                <CommentIcon commentsLen={post.comments.length} />
-              </CardActions>
-            </>
-          )}
-        </Card>
+        <SinglePost
+          posts={post}
+          user={user}
+          commentsLen={comments.commentsList.length}
+          hasLink={false}
+        />
       </Container>
       <Container className={classes.comment}>
         <Card>
           <CardContent>
-            <CreateComment postId={postid} userId={userid} />
+            {user ? (
+              <CreateComment postId={postid} userId={user._id} />
+            ) : (
+              'Login to comment'
+            )}
           </CardContent>
         </Card>
       </Container>
