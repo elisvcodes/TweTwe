c725d1eb52d2d45bf47a563cde434cd89b2b2470
elisvcodes <sultanoveli@outlook.com>
2021-04-13 17:27:29 -0400

    Slight modification

--- client/src/components/posts/SinglePostData.js
+++ client/src/components/posts/SinglePostData.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import { CardHeader, CardContent, Typography } from '@material-ui/core';
+import {
+  CardHeader,
+  CardContent,
+  Typography,
+  Link,
+  makeStyles,
+} from '@material-ui/core';
+import DeletePost from './DeletePost';
 import * as dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-export default function SinglePostData({ post }) {
+const useStyles = makeStyles({
+  link: {
+    color: 'black',
+  },
+});
+
+export default function SinglePostData({ post, hasLink }) {
+  const classes = useStyles();
   return (
     <>
       <CardHeader
-        title={post.author.name}
+        title={
+          <Link
+            href={`/${post.author._id}`}
+            underline="none"
+            className={classes.link}
+          >
+            {post.author.name}
+          </Link>
+        }
         subheader={<p> {dayjs(post.createdAt).format('MM/DD/YYYY')}</p>}
+        action={<DeletePost postId={post._id} />}
       />
       <CardContent>
-        <Typography variant="h6">{post.article}</Typography>
+        {hasLink ? (
+          <Link
+            href={`/${post.author._id}/post/${post._id}`}
+            underline="none"
+            className={classes.link}
+          >
+            <Typography variant="h6">{post.article}</Typography>
+          </Link>
+        ) : (
+          <Typography variant="h6">{post.article}</Typography>
+        )}
       </CardContent>
     </>
   );
