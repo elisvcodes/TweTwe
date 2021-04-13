1b86c52f4b20101bb8cf404ca906be4377bbaf05
elisvcodes <sultanoveli@outlook.com>
2021-04-13 18:18:04 -0400

    captain-definition

--- server/app.js
+++ server/app.js
@@ -5,10 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 8000;
+
 app.use('/auth', require('./routes/auth'));
 app.use('/users', require('./routes/user'));
 app.use('/posts', require('./routes/post'));
 app.use('/comments', require('./routes/comment'));
 app.use('/follow', require('./routes/follow'));
 app.use('/search', require('./routes/search'));
-app.listen(8000);
+app.listen(PORT);
