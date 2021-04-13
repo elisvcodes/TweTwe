a3a1aea6abf9af30fe340eda29cce20534d26056
elisvcodes <sultanoveli@outlook.com>
2021-04-13 17:21:24 -0400

    removed unused reducer

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -3,13 +3,11 @@ import auth from './auth';
 import user from './user';
 import posts from './posts';
 import follow from './follow';
-import post from './singlePost';
 import comments from './comments';
 export default combineReducers({
   auth,
   user,
   posts,
   follow,
-  post,
   comments,
 });
