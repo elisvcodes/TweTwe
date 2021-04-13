a65f6bdd18598b2acc84d999e652daee93322172
elisvcodes <sultanoveli@outlook.com>
2021-04-13 17:21:39 -0400

    removed this file

--- client/src/_reducers/singlePost.js
+++
@@ -1,8 +0,0 @@
-export default (post = {}, action) => {
-  switch (action.type) {
-    case 'GET_SINGLE_POST':
-      return { ...post, ...action.payload };
-    default:
-      return post;
-  }
-};
